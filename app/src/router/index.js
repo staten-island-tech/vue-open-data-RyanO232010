1648b8b139909b86c588cfbe30b60806bcc224ec
RyanO232010 <ryano92@nycstudents.net>
2026-03-16 12:32:17 -0400

    1

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,8 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from '@/views/MainPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: MainPage,
+    },
+  ],
 })
 
 export default router
---
+++ app/src/views/MainPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import PokemonCard from '@/components/PokemonCard.vue'
+
+const pokemon = ref([])
+async function getPokemon() {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=151&offset=0`)
+    const data = await response.json()
+    pokemon.value = data.results
+  } catch (error) {
+    console.log(error)
+  }
+}
+onMounted(() => {
+  getPokemon()
+})
+</script>
+
+<style scoped>
+
+</style>
